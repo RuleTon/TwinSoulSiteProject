8a92db4c2ca4671d58198107f6e628798a9ad869
Ruleton-at-301701292774 <catalex777@gmail.com>
2021-10-06 00:16:34 +0300

    Initial commit

---
+++ src/main/java/com/finalproject/TwinSoul/TwinSoulApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.TwinSoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwinSoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwinSoulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/TwinSoul/TwinSoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.TwinSoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwinSoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
