79907afa7547c51a22291caaf03a163a9149db29
Ruleton-at-301701292774 <catalex777@gmail.com>
2021-10-13 00:03:05 +0300

    Add BD connection, some new pages, Create, Edit and Delete

---
+++ src/main/java/com/finalproject/TwinSoul/repo/PostRepository.java
@@ -0,0 +1,7 @@
+package com.finalproject.TwinSoul.repo;
+
+import com.finalproject.TwinSoul.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+}
